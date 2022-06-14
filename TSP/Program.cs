b87f3f7ec7fec7e7e84a34e946ba5c191ac44fed
dfoltynski <dawidfoltynski7@gmail.com>
2022-06-14 23:46:01 +0200

    Added travelling salesman problem

---
+++ TSP/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+class Program
+{
+  public static Random rand = new Random();
+
+  public static void Main(string[] args)
+  {
+
+    System.Console.Write("Podaj ilosc miast: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int[] cities = populate(num);
+    int[] gene = cities.OrderBy(_ => rand.Next()).Take(cities.Length).ToArray();
+
+    int[,] costTable = generateRandomCostTable(num);
+    int? currentMinCost = 0;
+    int currentCost = 0;
+    int counter = 1;
+    displayArray(cities);
+    displayArray(gene);
+    displayArray(costTable, num);
+
+    while (counter != num)
+    {
+      currentCost = 0;
+      counter++;
+
+      for (int j = 0; j < num; j++)
+      {
+        if ((j + 1) >= num)
+        {
+          break;
+        }
+        else
+        {
+          if (currentMinCost == 0)
+            currentMinCost += costTable[gene[j] - 1, gene[j + 1] - 1];
+          else
+          {
+            currentCost += costTable[gene[j] - 1, gene[j + 1] - 1];
+          }
+        }
+      }
+      System.Console.Write($"Current gene: ");
+      displayArray(gene);
+      System.Console.WriteLine($"Current minimum cost: {currentCost}");
+
+      if (currentCost < currentMinCost)
+        currentMinCost = currentCost;
+      swapGenes(gene);
+
+    }
+    System.Console.Write($"The best gene is: ");
+    displayArray(gene);
+  }
+
+  private static void swapGenes(int[] arr)
+  {
+    int firstIndexToSwap = rand.Next(0, arr.Length - 1);
+    int secondIndexToSwap = rand.Next(0, arr.Length - 1);
+    int firstElement = arr[firstIndexToSwap];
+    int secondElement = arr[secondIndexToSwap];
+
+    arr[firstIndexToSwap] = secondElement;
+    arr[secondIndexToSwap] = firstElement;
+  }
+
+  private static void displayArray(int[] arr)
+  {
+    foreach (var item in arr)
+    {
+      System.Console.Write($"{item}, ");
+    }
+    System.Console.WriteLine();
+  }
+
+  private static void displayArray(int[,] arr, int length)
+  {
+    for (int i = 0; i < length; i++)
+    {
+      for (int j = 0; j < length; j++)
+      {
+        System.Console.Write($"{arr[i, j]},");
+      }
+      System.Console.WriteLine();
+    }
+  }
+
+  private static int[,] generateRandomCostTable(int num)
+  {
+    int[,] twoDimArr = new int[num, num];
+    for (int i = 0; i < num; i++)
+    {
+      for (int j = 0; j < num; j++)
+      {
+        twoDimArr[i, j] = rand.Next(1, 30);
+      }
+    }
+
+    return twoDimArr;
+  }
+
+  private static int[] populate(int num)
+  {
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+      arr[i] = i + 1;
+    }
+
+    return arr;
+  }
+
+  // private static int[] generateRandomGene(int num, int[] cities)
+  // {
+  //   Random rand = new Random();
+
+  //   int[] arr = new int[num];
+  //   for (int i = 0; i < num; i++)
+  //   {
+  //     // arr[i] = cities.OrderBy(_ => rand.Next()).Take(4).ToArray();
+  //   }
+
+  //   return arr;
+  // }
+}
--- equations/Program.cs
+++ equations/Program.cs
@@ -1,65 +1,196 @@
-﻿using System;
+﻿// int[] b = new int[] { 1, 2, 3, 4 };
+// int[] a = new int[] { 5, 6, 7, 8 };
 
-class Program
+
+// Stack<int> S = new Stack<int>(a);
+// Queue<int> Q = new Queue<int>(b);
+
+// // Q.Enqueue(S.Peek()); S.Pop();
+// // S.Push(Q.Peek()); Q.Dequeue();
+// // S.Push(Q.Peek()); Q.Dequeue();
+// // Q.Enqueue(S.Peek()); S.Pop();
+// // Q.Enqueue(S.Peek()); S.Pop();
+// // S.Push(Q.Peek()); Q.Dequeue();
+
+// S.Push(Q.Peek()); Q.Dequeue();
+// S.Push(Q.Peek()); Q.Dequeue();
+// S.Push(Q.Peek()); Q.Dequeue();
+// Q.Enqueue(S.Peek()); S.Pop()
+
+// foreach (var item in S)
+// {
+//   System.Console.Write($"{item} ");
+// }
+// System.Console.WriteLine();
+// foreach (var item in Q)
+// {
+//   System.Console.Write($"{item} ");
+// }
+
+// int wynik = 0;
+
+// int oblicz(int a, int b, int c) {
+
+// }
+
+// int[] tab;
+// System.Console.WriteLine(tab.Count());
+// namespace asdd{
+// private interface asd {
+
+// }
+// }
+
+// int[] a = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), int.Parse);
+
+// int n = a.Length;
+
+// string answer = "";
+
+// int i = 0;
+// int j = 0;
+// int minindex = 0;
+// int tmp;
+// while (i < n - 1)
+// {
+//   if (i < n - 1)
+//   {
+//     minindex = i;
+//     j = i + 1;
+
+//     if (j < n)
+//     {
+//       if (a[j] < a[minindex])
+//       {
+//         minindex = j;
+//       }
+//       j++;
+//     }
+//     else
+//     {
+//       tmp = a[i];
+
+//       a[i] = a[minindex];
+//       a[minindex] = tmp;
+//       i++;
+//     }
+//   }
+//   if (!(i < n - 1))
+//     foreach (var item in a)
+//     {
+//       answer += $"{a}, ";
+//       System.Console.Write($"{item}, ");
+//     }
+//   // {
+//   n--;
+// }
+// }
+
+// answer = answer.Trim();
+// System.Console.WriteLine(answer.Remove(answer.Length - 1));
+
+int[] arr = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), int.Parse);
+int temp;
+for (int j = 0; j <= arr.Length - 2; j++)
 {
-  public static void Main(string[] args)
+  for (int i = 0; i <= arr.Length - 2; i++)
   {
-    Console.Clear();
-
-    bool exit = false;
-
-    while (!exit)
+    if (arr[i] > arr[i + 1])
     {
-      Console.WriteLine("Prosze wybrać równanie do rozwiązania");
-      Console.WriteLine("1. Równanie trzeciego stopnia [3]");
-      Console.WriteLine("2. Równanie czwartego stopnia [3.5]");
-      Console.WriteLine("3. Równanie piątego stopnia / Metoda połowienia przedziałów [4]");
-      Console.WriteLine("4. Algorytm genetyczny [4.5]");
-      Console.WriteLine("5. ???? [5]");
-      Console.WriteLine("");
-      Console.WriteLine("Aby wyjść proszę wpisać exit / e, albo kliknąć ctrl+c");
-
-      System.Console.Write("Wybór: ");
-      string answer = Console.ReadLine();
-
-      if (answer == "e" || answer == "exit")
-      {
-        exit = true;
-      }
-      else if (int.TryParse(answer, out int a))
-      {
-        Console.Clear();
-        switch (a)
-        {
-          case 1:
-            System.Console.WriteLine("Rownanie 3 stopnia");
-            break;
-
-          case 2:
-            System.Console.WriteLine("Rownanie 4 stopnia");
-            break;
-
-          case 3:
-            System.Console.WriteLine("Rownanie 5 stopnia / metoda polowienia przedzialow");
-            break;
-
-          case 4:
-            System.Console.WriteLine("Algorytm genetyczny");
-            break;
-
-          case 5:
-            System.Console.WriteLine("????");
-            break;
-
-          default:
-            break;
-        }
-      }
-      else
-      {
-        System.Console.Clear();
-        System.Console.WriteLine("Podano złą wartość");
-      }
+      temp = arr[i + 1];
+      arr[i + 1] = arr[i];
+      arr[i] = temp;
     }
   }
 }
+for (int i = 0; i < arr.Length; i++)
+{
+  if (i != arr.Length - 1)
+  {
+    Console.Write(arr[i] + ", ");
+  }
+  else
+    Console.Write(arr[i]);
+}
+
+
+
+
+// int fun(int n)
+// {
+//   if (n < 2) return n;
+//   if (n % 2 == 1) return fun(n - 1) + 1;
+//   else return fun(n - 1);
+// }
+
+// System.Console.WriteLine(fun(7));
+// System.Console.WriteLine(fun(8));
+// System.Console.WriteLine(fun(9));
+
+// System.Console.WriteLine("1" + " " == "1");
+
+// using System;
+
+// class Program
+// {
+//   public static void Main(string[] args)
+//   {
+//     Console.Clear();
+
+//     bool exit = false;
+
+//     while (!exit)
+//     {
+//       Console.WriteLine("Prosze wybrać równanie do rozwiązania");
+//       Console.WriteLine("1. Równanie trzeciego stopnia [3]");
+//       Console.WriteLine("2. Równanie czwartego stopnia [3.5]");
+//       Console.WriteLine("3. Równanie piątego stopnia / Metoda połowienia przedziałów [4]");
+//       Console.WriteLine("4. Algorytm genetyczny [4.5]");
+//       Console.WriteLine("5. ???? [5]");
+//       Console.WriteLine("");
+//       Console.WriteLine("Aby wyjść proszę wpisać exit / e, albo kliknąć ctrl+c");
+
+//       System.Console.Write("Wybór: ");
+//       string answer = Console.ReadLine();
+
+//       if (answer == "e" || answer == "exit")
+//       {
+//         exit = true;
+//       }
+//       else if (int.TryParse(answer, out int a))
+//       {
+//         Console.Clear();
+//         switch (a)
+//         {
+//           case 1:
+//             System.Console.WriteLine("Rownanie 3 stopnia");
+//             break;
+
+//           case 2:
+//             System.Console.WriteLine("Rownanie 4 stopnia");
+//             break;
+
+//           case 3:
+//             System.Console.WriteLine("Rownanie 5 stopnia / metoda polowienia przedzialow");
+//             break;
+
+//           case 4:
+//             System.Console.WriteLine("Algorytm genetyczny");
+//             break;
+
+//           case 5:
+//             System.Console.WriteLine("????");
+//             break;
+
+//           default:
+//             break;
+//         }
+//       }
+//       else
+//       {
+//         System.Console.Clear();
+//         System.Console.WriteLine("Podano złą wartość");
+//       }
+//     }
+//   }
+// }
